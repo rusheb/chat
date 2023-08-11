ffc72d98221a6d1c9ce60e1e6793cc8cbaf19d89
Rusheb <rusheb.shah@gmail.com>
2023-08-11 23:16:06 +0100

    WIP

--- tests/test_server.py
+++ tests/test_server.py
@@ -13,9 +13,11 @@ async def test_server_can_accept_client():
     # users = {}
     server = ChatServer()
     server_task = asyncio.create_task(server.start())
-    await asyncio.sleep(1)
+    await asyncio.sleep(0.1)
 
-    await asyncio.open_connection("127.0.0.1", 8888)
+    _, writer = await asyncio.open_connection("127.0.0.1", 8888)
+    writer.write("test\n".encode())
+    await asyncio.sleep(0.1)
 
-    # assert len(users) == 1
-    # server_task.cancel()
+    assert len(server.users) == 1
+    server_task.cancel()
