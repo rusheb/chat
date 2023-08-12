f49e788c96ed3d4939a292bc81c3d4cb17efcc42
Rusheb <rusheb.shah@gmail.com>
2023-08-12 01:10:08 +0100

    Exit if wrong args are given

--- chat_client.py
+++ chat_client.py
@@ -6,6 +6,7 @@ from chat.client import start_chat_client
 if __name__ == '__main__':
     if not len(sys.argv) == 2:
         print("USAGE: client.py <name>")
+        sys.exit(1)
 
     client_name = sys.argv[1]
     asyncio.run(start_chat_client(client_name))
